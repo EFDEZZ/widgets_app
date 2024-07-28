defe09c92b515e5deb253e40af73a1ce4c77cd44
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-07-27 22:04:01 -0400

    Home Screen Creado

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Widget APP"),
+      ),
+      body: const Text("Hola"),
+    );
+  }
+}

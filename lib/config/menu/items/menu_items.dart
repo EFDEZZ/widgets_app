a9aa53c7f74b3e9e7dffbfecb4bd0125c1c32f59
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-07-31 23:24:02 -0400

    animated container terminado

--- lib/config/menu/items/menu_items.dart
+++ lib/config/menu/items/menu_items.dart
@@ -38,4 +38,10 @@ List<MenuItems> appMenuItems = [
     links: "/snackbar", 
     icon: Icons.info_outline,
     ),
+  MenuItems(
+    title: "Animated Container", 
+    subtitle: "Statefull widget animado", 
+    links: "/animated", 
+    icon: Icons.check_box_outline_blank_outlined,
+    ),
 ];
